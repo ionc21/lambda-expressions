f058ca8a0b709b27bd8e13f9832213471a4d875f
Ion Ciobanu <iciobanu@pgs-soft.com>
2017-09-07 14:33:23 +0200

    Executor and Callable interfaces

---
+++ src/main/java/com/lambda/executor/PlayingWithCallablesAndFutures.java
@@ -0,0 +1,28 @@
+package com.lambda.executor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class PlayingWithCallablesAndFutures {
+
+	public static void main(final String[] args) throws ExecutionException, InterruptedException {
+
+		Callable<String> task = () -> {
+			throw new IllegalStateException("I throw an exception in thread " + Thread.currentThread().getName());
+		};
+
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+
+		try {
+			for (int i = 0; i < 10; i++) {
+				Future<String> future = executor.submit(task);
+				System.out.println("I get: " + future.get());
+			}
+		} finally {
+			executor.shutdown();
+		}
+	}
+}
--- src/main/java/com/lambda/functional/Comparator.java
+++ src/main/java/com/lambda/functional/Comparator.java
@@ -1,15 +1,13 @@
 package com.lambda.functional;
 
-import com.lambda.bo.Person;
-
 import java.util.function.Function;
 
 @FunctionalInterface
 public interface Comparator<T> {
 
-    public int compare(T t1, T t2);
+	public int compare(T t1, T t2);
 
-    public static <U> Comparator<U> comparing(Function<U, Comparable> f) {
-        return (p1, p2) -> f.apply(p1).compareTo(f.apply(p2));
-    }
+	public static <U> Comparator<U> comparing(final Function<U, Comparable> f) {
+		return (p1, p2) -> f.apply(p1).compareTo(f.apply(p2));
+	}
 }
